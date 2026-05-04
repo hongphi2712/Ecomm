a161e9a5e84c89475d2331d4c3e9e79ea958107d
Phi Hong <11236179@st.neu.edu.vn>
2026-05-04 15:37:59 +0700

    chore: remove frontend from repository as requested

--- frontend/.next/dev/static/chunks/frontend_src_app_(auth)_login_page_tsx_01-ytzy._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0r9g_03s~pdl._.js","static/chunks/frontend_src_0ux80am._.js"],
-    source: "dynamic"
-});
